f6df67bed07eb3ca664c2941132a0ea7840d2c15
Rob Knopf <rknopf@whirlinggizmo.com>
2024-07-02 15:20:33 -0400

    added DropShadowActor sample

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { Resources, loader } from "./resources";
 import { Treetop } from "./treetop";
 import { createDropShadowMaterial } from "./dropshadowmaterial";
 import { isMobile } from "./util/platform";
+import { DropShadowActor } from "./dropshadowactor";
 
 function addLabel(actor: Actor, text: string) {
   const label = new Label({
@@ -38,29 +39,41 @@ class Game extends Engine {
     treetop_no_shadow.pos.y = currentYPosition;
     this.add(treetop_no_shadow);
     addLabel(treetop_no_shadow, 'No shadow');
-    currentYPosition += treetop_no_shadow.width + verticalSpacing;
+    currentYPosition += treetop_no_shadow.height + verticalSpacing;
     // Note: no drop shadow material set
 
     // treetop, trimmed sprite + shadow material
     const treetop_trimmed_shadow = new Treetop(Resources.TreetopTrimmed);
     treetop_trimmed_shadow.pos.y = currentYPosition;
     this.add(treetop_trimmed_shadow);
-    addLabel(treetop_trimmed_shadow, 'Trimmed sprite\n(No empty space around sprite, shadow is clipped)');
-    currentYPosition += treetop_trimmed_shadow.width + verticalSpacing;
+    addLabel(treetop_trimmed_shadow, 'Trimmed Sprite + DropShadowMaterial\n - No empty space around sprite\n - Shadow is clipped');
+    currentYPosition += treetop_trimmed_shadow.height + verticalSpacing;
     // set the drop shadow material
     treetop_trimmed_shadow.graphics.material = dropShadowMaterial;
 
     // treetop, untrimmed sprite + shadow material
-    const treetop_not_trimmed = new Treetop(Resources.TreetopNotTrimmed);
-    treetop_not_trimmed.pos.y = currentYPosition;
-    this.add(treetop_not_trimmed);
-    addLabel(treetop_not_trimmed, 'Untrimmed sprite\n(empty space around sprite, shadow is not clipped)');
-    currentYPosition += treetop_not_trimmed.width * 2;
+    const treetop_not_trimmed_shadow = new Treetop(Resources.TreetopNotTrimmed);
+    treetop_not_trimmed_shadow.pos.y = currentYPosition;
+    this.add(treetop_not_trimmed_shadow);
+    addLabel(treetop_not_trimmed_shadow, 'Untrimmed Sprite + DropShadowMaterial\n - Extra empty space around sprite\n - Shadow is not clipped');
+    currentYPosition += treetop_not_trimmed_shadow.height + verticalSpacing;
     // set the drop shadow material
-    treetop_not_trimmed.graphics.material = dropShadowMaterial;
+    treetop_not_trimmed_shadow.graphics.material = dropShadowMaterial;
 
-    // look at the middle tree for centering
-    this.currentScene.camera.strategy.lockToActor(treetop_trimmed_shadow);
+    // add a DropShadowActor to compare
+    const dropShadowActor = new DropShadowActor({
+      pos: vec(0, currentYPosition),
+      width: 64,
+      height: 64
+    });
+    dropShadowActor.graphics.add(Resources.TreetopTrimmed.toSprite());
+    this.add(dropShadowActor);
+    addLabel(dropShadowActor, 'DropShadowActor\n - No DropShadowMaterial\n - No empty space around sprite\n - Shadow is not clipped');
+    currentYPosition += treetop_no_shadow.height + verticalSpacing;
+
+
+    // look at the third tree for centering
+    this.currentScene.camera.strategy.lockToActor(treetop_not_trimmed_shadow);
 
     this.start(loader);
   }
